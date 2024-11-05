766ca46db75c7c182c66ceb5f49721a3f6033ab4
Lindokuhle Sibeko <138812996+TheBoyBass@users.noreply.github.com>
2024-11-05 11:00:42 +0200

    Removed Repeating Code

--- scripts.js
+++ scripts.js
@@ -32,6 +32,7 @@ const createElement = (tag, className, attributes = {}, innerHTML = '') => {
     return element;
 };
 
+// 
 const renderBooks = (booksToRender) => {
     const fragment = document.createDocumentFragment();
     booksToRender.forEach(({ author, id, image, title }) => {
@@ -99,26 +100,6 @@ const init = () => {
 init();
 
 const starting = document.createDocumentFragment()
-
-for (const { author, id, image, title } of matches.slice(0, BOOKS_PER_PAGE)) {
-    const element = document.createElement('button')
-    element.classList = 'preview'
-    element.setAttribute('data-preview', id)
-
-    element.innerHTML = `
-        <img
-            class="preview__image"
-            src="${image}"
-        />
-        
-        <div class="preview__info">
-            <h3 class="preview__title">${title}</h3>
-            <div class="preview__author">${authors[author]}</div>
-        </div>
-    `
-
-    starting.appendChild(element)
-}
 
 document.querySelector('[data-list-items]').appendChild(starting)
 
